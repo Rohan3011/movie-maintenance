87cea42d2e89a8cccb189390de35540839c5d0b0
Rohanop <rohankamble3011@gmail.com>
2022-04-19 15:53:27 +0530

    Add: movie-card

---
+++ src/app/components/movie-card/movie-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.css']
+})
+export class MovieCardComponent implements OnInit {
+  title: string = "Movie name";
+  releaseDate: string = "33-33-22"
+  image = "https://images.pexels.com/photos/5662857/pexels-photo-5662857.png?cs=srgb&dl=pexels-tima-miroshnichenko-5662857.jpg&fm=jpg"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
