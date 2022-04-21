0ff42141558c3e2bd5edc5d6a8cfb05a7c09ad9f
Rohanop <rohankamble3011@gmail.com>
2022-04-21 14:21:52 +0530

    Add: filter icon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowDownShortWide } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title = "Movie Name"
-
+  filter = faArrowDownShortWide;
   constructor() { }
 
   ngOnInit(): void {
