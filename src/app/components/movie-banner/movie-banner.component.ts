1813f3d0c6da5ab26639a4b27538b9f3c18e952e
Rohanop <rohankamble3011@gmail.com>
2022-04-21 13:41:31 +0530

    Add: actions bar

--- src/app/components/movie-banner/movie-banner.component.ts
+++ src/app/components/movie-banner/movie-banner.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from 'src/app/services/movie.service';
 import { Movie } from 'src/app/types/Movie';
-
+import { faThumbsUp, faThumbsDown, faClock, faArrowUpFromBracket, faEllipsis } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-movie-banner',
@@ -11,14 +11,13 @@ import { Movie } from 'src/app/types/Movie';
 })
 export class MovieBannerComponent implements OnInit {
 
-  movie: Movie = {
-    date_of_release: "",
-    title: "",
-    image: "",
-    id: 0,
-    language: "",
-    description: ""
-  }
+  movie!: Movie;
+
+  like = faThumbsUp;
+  dislike = faThumbsDown;
+  later = faClock;
+  share = faArrowUpFromBracket;
+  more = faEllipsis;
 
   public movieId: string = "";
   constructor(private movieService: MovieService, route: ActivatedRoute) {
